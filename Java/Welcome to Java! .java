d2221d36f2f757f3836c51ec84d1901fdf407464
LIZA1608 <78140921+LIZA1608@users.noreply.github.com>
2021-07-16 14:45:59 +0530

    Create Welcome to Java! .java

---
+++ Java/Welcome to Java! .java
@@ -0,0 +1,11 @@
+/*https://www.hackerrank.com/challenges/welcome-to-java/problem
+Author -    Shafaet
+Difficulty -Easy
+Max Score - 3 */
+public class Solution {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, World.");
+        System.out.println("Hello, Java.");
+}
+}
