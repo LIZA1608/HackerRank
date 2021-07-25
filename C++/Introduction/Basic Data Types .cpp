7d9c4143c713032052474cdda0166780eed6c9d9
LIZA1608 <78140921+LIZA1608@users.noreply.github.com>
2021-07-25 15:16:22 +0530

    Create Basic Data Types .cpp

---
+++ C++/Introduction/Basic Data Types .cpp
@@ -0,0 +1,16 @@
+/*  https://www.hackerrank.com/challenges/c-tutorial-basic-data-types/problem      */
+
+#include <iostream>
+#include <cstdio>
+int main()
+{
+int a;
+long int b;
+
+char c;
+float e;
+double f;
+scanf("%d %ld  %c %f %lf",&a,&b,&c,&e,&f);
+printf("%d\n%ld\n%c\n%f\n%lf",a,b,c,e,f);
+return 0;
+}
