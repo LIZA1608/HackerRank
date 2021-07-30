c9d3c1348820720710bc28ff0315c52a65dc5fe7
LIZA1608 <78140921+LIZA1608@users.noreply.github.com>
2021-07-30 13:39:58 +0530

    Update Say "Hello, World!" With C++ .cpp

--- C++/Introduction/Say "Hello, World!" With C++ .cpp
+++ C++/Introduction/Say "Hello, World!" With C++ .cpp
@@ -1,9 +1,7 @@
 /* https://www.hackerrank.com/challenges/cpp-hello-world/problem */
 
 #include <iostream>
-#include <cstdio>
 using namespace std;
-
 int main() {
  cout<<"Hello, World!";
     return 0;
