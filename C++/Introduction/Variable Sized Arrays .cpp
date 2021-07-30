ad081d9909a08878bffd718363311bf8571e83bd
LIZA1608 <78140921+LIZA1608@users.noreply.github.com>
2021-07-30 13:40:42 +0530

    Update Variable Sized Arrays .cpp

--- C++/Introduction/Variable Sized Arrays .cpp
+++ C++/Introduction/Variable Sized Arrays .cpp
@@ -6,9 +6,6 @@
 #include <iostream>
 #include <algorithm>
 using namespace std;
-
-
-
 int main() {
     /* Enter your code here. Read input from STDIN. Print output to STDOUT */   
     int n,q,k,i,j;
@@ -21,11 +18,9 @@ int main() {
             cin >> arr[i][j];
         }       
     }
-
     for(int l=0; l<q; ++l){
         cin >> i >> j;
         cout << arr[i][j]<<endl;
     }  
-        
-    return 0;
+  return 0;
 }
