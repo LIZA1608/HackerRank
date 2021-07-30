c767877ef636ebbdca58555541d283ac033d01c8
LIZA1608 <78140921+LIZA1608@users.noreply.github.com>
2021-07-30 13:39:11 +0530

    Update Input and Output .cpp

--- C++/Introduction/Input and Output .cpp
+++ C++/Introduction/Input and Output .cpp
@@ -1,10 +1,5 @@
 /*https://www.hackerrank.com/challenges/cpp-input-and-output/problem */
-
-#include <cmath>
-#include <cstdio>
-#include <vector>
 #include <iostream>
-#include <algorithm>
 using namespace std;
 int main() {
 int a,b,c,d;
